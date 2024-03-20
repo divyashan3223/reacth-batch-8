58cdf3eabfb20ab0e4931ae2b8f0805d9376710d
frenzo technology <divyashan3223@gmail.com>
2024-03-20 11:56:05 +0530

    useref

--- src/components/hooks/AddingCount.jsx
+++ src/components/hooks/AddingCount.jsx
@@ -1,24 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const AddingCount = () => {
-  const [count, setCount] = useState(0);
-  const [count1, setCount1] = useState(10);
-  function handleClick() {
-    setCount(count + 1);
-  }
-  function handleClickminus() {
-    setCount1(count1 - 1);
-  }
-  useEffect(() => {
-    console.log("runs all state change");
-  }, [count1]);
+  const inputRef = useRef(10);
+  console.log(inputRef);
+  // useEffect(() => {
+  //   inputRef.current = inputRef.current + 1;
+  //   alert("you clikck" + inputRef.current + "times");
+  // });
+  let handleClick = () => {
+    inputRef.current = inputRef.current + 1;
+    // alert("you clikck" + inputRef.current + "times");
+    console.log(inputRef.current);
+  };
+
   return (
     <div>
-      <p>count:{count}</p>
-      <p>count:{count1}</p>
-
-      <button onClick={handleClick}>add num</button>
-      <button onClick={handleClickminus}>minus num</button>
+      <button onClick={handleClick}>click me</button>
     </div>
   );
 };
